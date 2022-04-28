990173d359e0fd0831b460dd7c6a524c11d0dc24
PuroLatexWolf <88239747+PuroLatexWolf@users.noreply.github.com>
2022-04-28 21:41:30 +0300

    Main
    Version 0.0.2P

---
+++ js/console.js
@@ -0,0 +1,10 @@
+let password = 0869 
+
+function ENTRYPASSWORD(pass) {
+    if (pass == 0869) {
+        console.log('1')
+    }
+    else {
+        console.log('0')
+    }
+} // im need your help
